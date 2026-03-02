c49f3937cb01a732e5e2469c70c79d11b9128d63
carolvillan2-ship-it <carolvillan2@gmail.com>
2026-03-02 18:12:43 -0400

    Create Tarea1
    Serie

---
+++ Tarea1.java
@@ -0,0 +1,16 @@
+package auxiliatura;
+
+public class Serie {
+    public String nombre; 
+    public String genero; 
+    private int nroEpisodios; 
+    private String [] episodios;
+    
+    public Serie (String x, String y, int z){
+        this.nombre = x; 
+        this.genero = y; 
+        this.nroEpisodios = z; 
+        
+    }
+     
+}
